f71219543c6ef5a133d53053290bad5c5ebf8a58
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2022-02-10 18:47:46 -0600

    r Rename `sut` to clarify the intent

--- Tests/PowerfulCombineTests/PowerfulCombineTestingTests.swift
+++ Tests/PowerfulCombineTests/PowerfulCombineTestingTests.swift
@@ -9,40 +9,40 @@ import XCTest
 final class PowerfulCombineTestingTests: XCTestCase {
     func test_immediateScheduleAction() {
         var isExecuted = false
-        let sut = DispatchQueue.testSchedule
+        let testScheduler = DispatchQueue.testSchedule
 
-        sut.schedule { isExecuted = true }
+        testScheduler.schedule { isExecuted = true }
         XCTAssertFalse(isExecuted, "Expected to no execute the schedule action")
 
-        sut.advance()
+        testScheduler.advance()
         XCTAssertTrue(isExecuted, "Expected to execute the schedule action")
     }
 
     func test_multipleImmediateScheduleActions() {
         var executionCallCount = 0
-        let sut = DispatchQueue.testSchedule
+        let testScheduler = DispatchQueue.testSchedule
 
-        sut.schedule { executionCallCount += 1 }
-        sut.schedule { executionCallCount += 1 }
+        testScheduler.schedule { executionCallCount += 1 }
+        testScheduler.schedule { executionCallCount += 1 }
 
         XCTAssertEqual(executionCallCount, 0, "Expected no actions executed")
 
-        sut.advance()
+        testScheduler.advance()
         XCTAssertEqual(executionCallCount, 2, "Expected actions executed")
     }
 
     func test_immediateScheduleWithPublisher() {
         var output = [String]()
         var cancellables = Set<AnyCancellable>()
-        let sut = DispatchQueue.testSchedule
+        let testScheduler = DispatchQueue.testSchedule
 
         Just("a value")
-            .receive(on: sut)
+            .receive(on: testScheduler)
             .sink(receiveValue: { output.append($0) })
             .store(in: &cancellables)
         XCTAssertEqual(output, [], "Expected no emmited values")
 
-        sut.advance()
+        testScheduler.advance()
         XCTAssertEqual(output, ["a value"], "Expected emmited value")
     }
 }
