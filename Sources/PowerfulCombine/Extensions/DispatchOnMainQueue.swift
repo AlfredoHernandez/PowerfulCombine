7a13994777c27c9e5527ecde265c2bda7d4bf3ed
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2022-02-16 16:44:52 -0600

    d Explains `dispatchOnMainQueue()`

--- Sources/PowerfulCombine/Extensions/DispatchOnMainQueue.swift
+++ Sources/PowerfulCombine/Extensions/DispatchOnMainQueue.swift
@@ -6,6 +6,9 @@ import Combine
 import Foundation
 
 public extension Publisher {
+    /**
+     * If on the main thread, execute work immediately. Else, dispatch the work asynchronously on the main dispatch queue.
+     */
     func dispatchOnMainQueue() -> AnyPublisher<Output, Failure> {
         receive(on: DispatchQueue.immediateWhenOnMainQueueScheduler).eraseToAnyPublisher()
     }
