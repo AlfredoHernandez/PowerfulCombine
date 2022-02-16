20c2c1518c72e8c0caaa24794252be07f12e5f45
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2022-02-16 16:52:11 -0600

    d Add docs for fallback(to:)

--- Sources/PowerfulCombine/Extensions/FallbackPublisher.swift
+++ Sources/PowerfulCombine/Extensions/FallbackPublisher.swift
@@ -6,6 +6,12 @@ import Combine
 import Foundation
 
 public extension Publisher {
+    /**
+     If first `Publisher` fails, then error is ignored and continue with a fallback version of it.
+     - Parameters:
+        - fallbackPublisher: A fallback version of the desired publisher
+     - Returns: An `AnyPublisher` with the same `Output` and `Failure`
+     */
     func fallback(to fallbackPublisher: AnyPublisher<Output, Failure>) -> AnyPublisher<Output, Failure> {
         self.catch { _ in fallbackPublisher }.eraseToAnyPublisher()
     }
